35fe4b9c7f3019d74c47501523a184671bfd6d03
Patrick Robertson <patricksrobertson@gmail.com>
2013-10-25 09:12:30 -0400

    Add VimRC stuff.

--- install.sh
+++ install.sh
@@ -2,10 +2,12 @@ rm -rf ~/.profile
 rm -rf ~/.bash_profile
 rm -rf ~/.bashrc
 rm -rf ~/.gitconfig
+rm -rf ~/.vimrc
 
 ln -s ~/my/dotfiles/bash/main.sh ~/.profile
 ln -s ~/.profile ~/.bash_profile
 ln -s ~/.profile ~/.bashrc
 ln -s ~/my/dotfiles/gitconfig ~/.gitconfig
+ln -s ~/my/dotfiles/vim/vimrc ~/.vimrc
 
 . ~/.profile
